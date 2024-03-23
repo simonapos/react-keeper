3f02594f332145f87dbbb93c120ee37753f2b6e5
Simona <simonapostolovska@gmail.com>
2024-03-23 02:24:41 +0100

    save progress

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,8 @@
-import { defineConfig, loadEnv } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
-  return {
-    define: {
-      'process.env.APP_KEY': JSON.stringify(env.APP_KEY),
-      'process.env.APP_ID': JSON.stringify(env.APP_ID),
-    },
-    plugins: [react()],
-  }
+export default defineConfig({
+  plugins: [react()],
+  base: "/react-keeper/"
 })
